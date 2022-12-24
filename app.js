6f3603e05779ba2a2da55ddd2b7a30d984008c82
faizanmalikk <faizanjamil656@gmail.com>
2022-12-25 00:38:53 +0500

    updated

--- app.js
+++ app.js
@@ -42,10 +42,10 @@ app.use('/api' ,  user)
 app.use('/api' ,  order)
 app.use('/api' ,  payment)
 
-app.use(express.static(path.join(__dirname,'../frontend/build')))
+app.use(express.static(path.join(__dirname,'./frontend/build')))
 
 app.get('*',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
+  res.sendFile(path.resolve(__dirname,'./frontend/build/index.html'))
 })
 
 // Middleware for errors
